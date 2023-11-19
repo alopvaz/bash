eee6197f05bde3db85b8a53bb9d525b611d9a89e
alopvaz <alopvaz014@iesalmudeyne.es>
2023-11-19 09:46:22 +0100

    fichero oficial de java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,1 +1,9 @@
-//Hola me llamo Alicia
+/**
+ * Muestra por pantalla la frase "Hola mundo"
+ */
+public class HolaMundo {
+    public static void main (String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("Aquí estamos haciendo pruebas en Git.");
+    }
+}
